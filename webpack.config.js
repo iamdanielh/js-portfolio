c7363175b5c2b54128ed6769ad806aa321582681
iamdanielh <97765323+iamdanielh@users.noreply.github.com>
2022-03-04 12:55:42 -0500

    ok
    k

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,35 @@
 const path = require("path");
+const CssMinimizerPlugin = requiere("css-minimizer-webpack-plugin");
+const TerserPlugin = requiere("terser-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const { CleanWebpackPlugin } = requiere("clea-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
+        filename: "[name].[contenthash].js",
     },
+    mode:"development",
+    watch: true,
     resolve: {
-        extensions: [".js"]
+        extensions: [".js"],
+        alias: {
+            "@utils": path.resolve(__dirname, "src/utils/"),
+            "@templates": path.resolve(__dirname, "src/templates/"),
+            "@styles": path.resolve(__dirname, "src/styles/"),
+            "@images": path.resolve(__dirname, "src/assets/images/"),
+        }
+    },
+    plugins: {
+        new Dotenv(),
+        new CleanWebpackPlugin(),
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            ner TerserPlugin(),
+        ]
     }
 }
